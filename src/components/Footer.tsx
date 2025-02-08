368b06d5e0f017cff536a28702909a195824c99c
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-02-08 14:05:34 +0000

    コンポーネントを分割しよう

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-blue-500 text-white text-sm py-4 text-center mt-auto">
+      &copy; 2023 full stack web development
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white text-lg font-bold py-4 px-6 flex justify-between items-center">
+      在庫管理システム
+    </header>
+  );
+};
+
+export default Header;
