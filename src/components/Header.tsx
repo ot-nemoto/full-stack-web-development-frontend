6c92cf53ba688e1d510ac327f95b82f4decd2b32
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-05-17 22:51:55 +0000

    devcontainer環境の立ち上げ

--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-blue-500 text-white text-lg font-bold py-4 px-6 flex justify-between items-center">
-      在庫管理システム
-    </header>
-  );
-}
