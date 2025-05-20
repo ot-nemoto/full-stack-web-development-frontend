d9111d0cc93288428d06ba2bea427ec63461cb3b
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-05-20 10:21:40 +0000

    動的描画の導入

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import productsData from "./sample/dummy_products.json";
+
+type ProductData = {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+};
+
 export default function Page() {
+  // 読込データを保持
+  const [data, setData] = useState<Array<ProductData>>([]);
+  useEffect(() => {
+    setData(productsData);
+  }, []);
+
   return (
     <>
       <h2>商品一覧</h2>
@@ -14,39 +32,17 @@ export default function Page() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>コットン100％バックリボンティアードワンピース（黒）</td>
-            <td>6900</td>
-            <td>
-              大人の愛らしさを引き立てる、ナチュラルな風合い。リラックス×トレンドを楽しめる、上品なティアードワンピース。
-            </td>
-            <td>
-              <button type="button">更新・削除</button>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>ライトストレッチカットソー（ネイビー）</td>
-            <td>2980</td>
-            <td>
-              しなやかな肌触りが心地よい、程よいフィット感のカットソー。ビジネスカジュアルにも普段使いにも使える、ベーシックなデザイン。
-            </td>
-            <td>
-              <button type="button">更新・削除</button>
-            </td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>ベルト付きデニムパンツ（ブルー）</td>
-            <td>5980</td>
-            <td>
-              定番のデニムパンツに、フェミニンなベルトをプラスしたスタイリッシュなアイテム。カジュアルにもきれいめにも合わせやすい。
-            </td>
-            <td>
-              <button type="button">更新・削除</button>
-            </td>
-          </tr>
+          {data.map((data: ProductData) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.name}</td>
+              <td>{data.price}</td>
+              <td>{data.description}</td>
+              <td>
+                <button type="button">更新・削除</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
